22d75dda8457c08861288432f62ef586a5a373d4
mercy cherono <cheronom142@gmail.com>
2024-10-05 16:21:13 +0300

    Rendering of all components completed

---
+++ react-travel-website/src/App.jsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+import Header from './Components/Header/Header'
+import Nav from './Components/Nav/Nav'
+import About from './Components/About/About'
+import Tours from './Components/Tours/Tours'
+import Country from './Components/Countries/Country'
+import Activities from './Components/Activities/Activities'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Blog from './Components/Blog/Blog'
+import Footer from './Components/Footer/Footer'
+
+function App() {
+  
+
+  return (
+    <>
+    <Nav/>
+    <Header/>
+    <About/>
+    <Tours/>
+    <Country/>
+    <Activities/>
+    <Testimonials/>
+    <Blog/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
